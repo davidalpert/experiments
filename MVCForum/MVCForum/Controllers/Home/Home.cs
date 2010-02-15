8b36ab83df5517316307eaed12da1ae943f19a52
David Alpert <david@spinthemoose.com>
2010-02-15 00:06:43 -0600

    performed the initial 'getting started' steps as outlined on http://guides.fubumvc.com/getting_started on February 14th 2010

---
+++ MVCForum/MVCForum/Controllers/Home/Home.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using FubuMVC.Core.View;
+
+namespace MVCForum.Controllers.Home
+{
+    public class HomeInputModel
+    { 
+    }
+
+    public class HomeViewModel
+    { 
+    }
+
+    public class HomeView : FubuPage<HomeViewModel>
+    { 
+    }
+
+    public class HomeController
+    {
+        public HomeViewModel Home(HomeInputModel input)
+        {
+            return new HomeViewModel();
+        }
+    }
+}
---
+++ MVCForum/MVCForum/Global.asax.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using FubuMVC.StructureMap.Bootstrap;
+
+namespace MVCForum
+{
+    public class Global : FubuStructureMapApplication
+    {
+    }
+}
